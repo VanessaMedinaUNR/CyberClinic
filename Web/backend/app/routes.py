eb3bfd6208cc0e126f3b8cfdc6a6ebde15a84535
Austin Finch <austinfinch@unr.edu>
2025-12-02 11:18:01 -0800

    fix backend import error and update dockerfile

--- Web/backend/app/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import jsonify, request
-from app import app
-
-@app.route('/')
-@app.route('/index', methods = ['GET'])
-def index():
-    data = "Hello World"
-    return jsonify({'data': data})
