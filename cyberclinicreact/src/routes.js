49a6e9299ea7c973c48d0bff875745e1e7470e52
Vanessa <vanessamedina2281@gmail.com>
2026-01-22 08:27:36 -0800

    added forgotpw page and routes VM 1/22/26

---
+++ cyberclinicreact/src/routes.js
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Forgotpw from "./forgotpw";
+import Login from "./login";
+
+export default function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/forgotpw" element={<Forgotpw />} />
+        </Routes>
+    );
+}
