4368bed819811903949a3684ee62fd8b0d27fe79
Vanessa <vanessamedina2281@gmail.com>
2026-01-22 16:16:43 -0800

    added signup page VM 1/22/26

--- cyberclinicreact/src/routes.js
+++ cyberclinicreact/src/routes.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Forgotpw from "./forgotpw";
 import Login from "./login";
+import Signup from "./signup";
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/forgotpw" element={<Forgotpw />} />
+            <Route path="/signup" element ={<Signup />} />
         </Routes>
     );
 }
