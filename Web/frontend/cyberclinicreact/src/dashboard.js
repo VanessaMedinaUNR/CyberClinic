b4f45d09141e4bcbcb7caa48c95d9640e115fb51
leslie Becerra <Lesliebecerra830@gmail.com>
2026-01-26 19:46:23 -0800

    React Dashboard

---
+++ Web/frontend/cyberclinicreact/src/dashboard.js
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import './dashboard.css';
+
+function Dashboard() {
+    const navigate = useNavigate();
+
+    async function handleSubmit(e){
+        e.preventDefault();
+    }
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+
+    }
+    console.log("client_id: " + getCookie("client_id"));
+    console.log("user_id: " + getCookie("user_id"));
+
+    return (
+        <div id="bounding_box">
+            <div className="dashboard-header">
+                <div className="brand-section">
+                    <h1>CyberClinic</h1>
+                    <p>University of Nevada, Reno</p>
+                </div>
+                <div className="user-controls">
+                    <span id="User-email"> </span>
+                    <button type = "button" onClick={() => (navigate("./settings"))} style={{ textDecoration: 'none', background: 'none', border: 'none', cursor: 'pointer' }}>⚙️</button>
+                </div>
+             </div>
+
+             <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px' }}>
+             <h1 style={{ fontSize: '20px', color: '#333', margin: 0 }}>Dashboard </h1>
+             <button className="btn-black" id = "newScan" onClick={() => (navigate("./newScan"))}>+ Configure New Scan 
+             </button>
+            </div>
+            <div className="content-card">
+                <div className="card-title-row">
+                    <h2>Active scan & Reports</h2>
+                </div>
+                <table id="scans-table">
+                    <thead>
+                        <tr>
+                            <th>Name / Target</th>
+                            <th>Date</th>
+                            <th> Security Rating </th>
+                            <th style={{ textAlign: 'right' }}>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
+export default Dashboard;
+
+   
+
+
+
--- Web/frontend/cyberclinicreact/src/setting.js
+++ Web/frontend/cyberclinicreact/src/setting.js
@@ -1,6 +1,10 @@
 // on html only copy paste what is in the body NOT THE SCRIPT 
 //copy the html body into the return then on top is where you will put the java/script 
 //when writing new code, html goes into return 
+//pushing through react: git add .
+//git commit -m " "
+//git push origin reactrefactor
+
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
