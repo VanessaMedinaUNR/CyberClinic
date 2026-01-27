7e65fd9eeaa1562e8e50ac4c5215f41a68af677e
Vanessa <vanessamedina2281@gmail.com>
2026-01-26 16:22:47 -0800

    added dashboard route VM 1/26/26

--- Web/frontend/cyberclinicreact/src/routes.js
+++ Web/frontend/cyberclinicreact/src/routes.js
@@ -3,6 +3,7 @@ import Forgotpw from "./forgotpw";
 import Login from "./login";
 import Signup from "./signup";
 import NewScan from "./newscan";
+import Dashboard from "./dashboard";
 
 export default function AppRoutes() {
     return (
@@ -11,6 +12,7 @@ export default function AppRoutes() {
             <Route path="/forgotpw" element={<Forgotpw />} />
             <Route path="/signup" element ={<Signup/>} />
             <Route path="/newscan" element ={<Signup/>} />
+            <Route path="/dashboard" element ={<Dashboard/>} />
         </Routes>
     );
 }
