3c3a94b953cffcfe7d1894a48e43afc675531013
Austin Finch <austinfinch@unr.edu>
2026-04-22 11:03:32 -0700

    location selector component bugfix to ensure data is being updated and sent to backend properly

--- Web/frontend/cyberclinicreact/src/Components/LocationSelector.js
+++ Web/frontend/cyberclinicreact/src/Components/LocationSelector.js
@@ -1,34 +1,47 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { CountryDropdown, StateDropdown, CityDropdown } from "react-country-state-dropdown";
 
 export default function LocationSelector({sendLocation}) {
-    const [location, setLocation] = useState({
+    const [country, setCountry] = useState(null);
+    const [state, setState] = useState(null);
+    const [city, setCity] = useState(null);
+    
+    const [loc, setLoc] = useState({
         country: '',
         state: '',
         city: '',
     });
 
-    const updateLocation = (field, value) =>
-    {
-        const newLocation = {...location, [field]: value};
-        setLocation(newLocation)
-        if (newLocation.country && newLocation.state && newLocation.city)
-        {
-            sendLocation(location)
-        }
+    const handleSetCountry = (e, c) => {
+        console.log(c)
+        setCountry(c);
+        setLoc({...loc, "country": c.iso2});
     };
+    const handleSetState = (e, st) => {
+        setState(st);
+        setLoc({...loc, "state": st.value})
+    };
+    const handleSetCity = (e, cit) => {
+        setCity(cit);
+        setLoc({...loc, "city": cit.value})
+    };
+
+    useEffect(() => {
+        sendLocation(loc);
+    }, [loc]);
+
     return <>
         <div>
             <label htmlFor="Country">Select a Country:</label>
-            <CountryDropdown value={location.country} onChange = {(val) => updateLocation("country",val)}required/>
+            <CountryDropdown value={country} onChange = {handleSetCountry}required/>
         </div>
         <div>
             <label htmlFor="State">Select a State:</label>
-            <StateDropdown country={location.country} value={location.state} onChange = {(val) => updateLocation("state",val)}required/>
+            <StateDropdown country={country} value={state} onChange = {handleSetState}required/>
         </div>
         <div>
             <label htmlFor="City">Select a City:</label>
-            <CityDropdown country={location.country} state={location.state} value={location.city} onChange = {(val) => updateLocation("city", val)}required/>
+            <CityDropdown country={country} state={state} value={city} onChange = {handleSetCity}required/>
         </div>
     </>
 }
